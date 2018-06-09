58dfc57fa4e7ae2d2dc8a2f32df45e8756a17383
kuba203020 <kuba203020@o2.pl>
2018-06-09 09:12:50 +0200

    Dokończenie listy

--- app/src/main/java/com/example/kuba/musicappkuba/topsongs/TopSongsActivity.java
+++ app/src/main/java/com/example/kuba/musicappkuba/topsongs/TopSongsActivity.java
@@ -3,14 +3,20 @@ package com.example.kuba.musicappkuba.topsongs;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.widget.Toast;
 
+
 import com.example.kuba.musicappkuba.R;
 import com.example.kuba.musicappkuba.api.ApiService;
 import com.example.kuba.musicappkuba.api.TrendingList;
+import com.example.kuba.musicappkuba.api.TrendingSingle;
 import com.google.gson.Gson;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -19,8 +25,8 @@ import retrofit2.Response;
 public class TopSongsActivity extends AppCompatActivity {
 
     RecyclerView rvList;
-
-    @Override
+    List<TrendingSingle> singles = new ArrayList<>( 0 );
+        @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_top_songs);
@@ -35,9 +41,14 @@ public class TopSongsActivity extends AppCompatActivity {
             @Override
             public void onResponse(@NonNull Call<TrendingList> call, @NonNull
                     Response<TrendingList> response) {
-                TrendingList trendingList;
-                trendingList = response.body();
-                Log. d ( "TAG" , new Gson().toJson(trendingList));
+                TrendingList TrendingList;
+                TrendingList = response.body();
+
+                if(TrendingList !=null && TrendingList.trending != null) {
+                    singles = TrendingList.trending;
+                }
+
+                setList();
             }
             @Override
             public void onFailure( @NonNull Call<TrendingList> call, Throwable t) {
@@ -45,6 +56,19 @@ public class TopSongsActivity extends AppCompatActivity {
                         t.getLocalizedMessage(), Toast. LENGTH_SHORT ).show();
             }
         });
+    }
+
+    private void setList() {
+        TopSongsAdapter topSongsAdapter = new TopSongsAdapter(singles);
+        rvList.setAdapter(topSongsAdapter);
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(  this);
+        linearLayoutManager.setReverseLayout(true);
+        linearLayoutManager.setStackFromEnd(true);
+
+        rvList.setLayoutManager(linearLayoutManager);
+
+        rvList.getAdapter().notifyDataSetChanged();
     }
 
     @Override
